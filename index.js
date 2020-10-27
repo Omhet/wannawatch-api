dbcd2a1497c459f49088b48a3fab44af12f5e445
Vladimir Ivanov <omhetehmo@yandex.ru>
2020-10-27 22:30:44 +0300

    Add mock user endpoint

--- index.js
+++ index.js
@@ -1,27 +1,40 @@
 import express from "express";
 
 const movies = [
-    {
-        id: '1',
-        name: 'Avatar',
-        year: 2008
-    },
-    {
-        id: '2',
-        name: 'Sherlock',
-        year: 2005
-    },
-]
+  {
+    id: "1",
+    name: "Avatar",
+    year: 2008,
+  },
+  {
+    id: "2",
+    name: "Sherlock",
+    year: 2005,
+  },
+];
+
+const users = [
+  {
+    id: "1",
+    name: "Joe",
+    movies: movies.slice(0, 1),
+  },
+  {
+    id: "2",
+    name: "Bob",
+    movies,
+  },
+];
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('<h1>Hello</h1>')
+app.get("/users/:id", (req, res) => {
+  const {
+    params: { id },
+  } = req;
+  const user = users.find((user) => user.id === id);
+  res.json(user);
 });
-
-app.get('/movies', (req, res) => {
-    res.json(movies)
-})
 
 const PORT = 3001;
 app.listen(PORT, () => console.log(`Server started on ${PORT}`));
