9b6f771cc102185e06277b73f78e1c13fe93089b
Vladimir Ivanov <omhetehmo@yandex.ru>
2020-10-27 23:12:01 +0300

    Add create user endpoint

--- index.js
+++ index.js
@@ -1,39 +1,40 @@
-import express from "express";
+import express from 'express';
 
 const movies = [
   {
-    id: "1",
-    name: "Avatar",
+    id: '1',
+    name: 'Avatar',
     year: 2008,
   },
   {
-    id: "2",
-    name: "Sherlock",
+    id: '2',
+    name: 'Sherlock',
     year: 2005,
   },
 ];
 
 let users = [
   {
-    id: "1",
-    name: "Joe",
+    id: '1',
+    name: 'Joe',
     movies: movies.slice(0, 1),
   },
   {
-    id: "2",
-    name: "Bob",
+    id: '2',
+    name: 'Bob',
     movies,
   },
 ];
 
 const app = express();
+app.use(express.json());
 
-app.get("/users/:id", (req, res) => {
+app.get('/users/:id', (req, res) => {
   const {
     params: { id },
   } = req;
-    const user = users.find((user) => user.id === id);
-    
+  const user = users.find((user) => user.id === id);
+
   if (user !== undefined) {
     res.json(user);
   } else {
@@ -41,13 +42,30 @@ app.get("/users/:id", (req, res) => {
   }
 });
 
-app.delete("/users/:id", (req, res) => {
+app.delete('/users/:id', (req, res) => {
   const {
     params: { id },
   } = req;
-    users = users.filter((user) => user.id !== id);
-    
-    res.status(204).end();
+  users = users.filter((user) => user.id !== id);
+
+  res.status(204).end();
+});
+
+app.post('/users', (req, res) => {
+  const maxId = users.length > 0 ? Math.max(...users.map(({ id }) => id)) : 0;
+  const {
+    body: { name },
+  } = req;
+  if (!name) {
+    return res.status(400).json({ error: 'name missing' });
+  }
+  const user = {
+    id: String(maxId + 1),
+    name,
+    movies: [],
+  };
+  users.push(user);
+  res.json(user);
 });
 
 const PORT = 3001;
